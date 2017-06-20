e7e16f4d9a6d154bb101a9f568b59a9052209286
DESKTOP-JVM2LBP\Reily <Reily.Halversen@clairtymis.com>
2017-06-20 18:30:00 -0500

    Need's content

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
   $('.gallery').slick({
-    dots: false,
+    dots: true,
     infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    pauseOnFocus: true,
+    pauseOnHover: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: false
         }
